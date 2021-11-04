cc9d17352bf5794a3b8a01191605058ef595b82c
Cybernetik <smxfr.cyb@gmail.com>
2021-11-05 00:43:16 +0100

    Update yt-dlp + quality choice before download

--- video.bat
+++ video.bat
@@ -1,6 +1,10 @@
 echo off
 :start
 set /p url="url : "
+yt-dlp.exe -F %url%
 set /p quality="quality : "
-youtube-dl.exe -f %quality% --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url%
+yt-dlp.exe -f %quality% --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url%
 GOTO start
+
+
+rem youtube-dl.exe -f %quality% --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url%
