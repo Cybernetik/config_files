6e3ac2b42bf5c0c6b1460a673bdd23c25d7aeabc
Cybernetik <smxfr.cyb@gmail.com>
2022-07-21 23:53:15 +0200

    Update video choice quality

--- video.bat
+++ video.bat
@@ -1,9 +1,14 @@
 echo off
 :start
 set /p url="url : "
-yt-dlp.exe -F %url%
+yt-dlp.exe -F %url% -v
 set /p quality="quality : "
-yt-dlp.exe -f %quality% --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url%
+if %quality% LEQ "best" GOTO best_choice
+:classic_choice
+yt-dlp.exe -f %quality% --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url% -v
+GOTO start
+:best_choice
+yt-dlp.exe -f bestvideo+bestaudio --ffmpeg-location "C:\bin" --output "D:\Video\%%(title)s.%%(ext)s" %url% -v
 GOTO start
 
 
